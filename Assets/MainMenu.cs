1a9b1bd60ffeac7b5cc4d9507c3c180d135b3427
kajackso <kajackso@mtu.edu>
2019-10-17 17:40:41 -0400

    bundling project together

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("PlayScreen");   //Load PlayScreen scene in the scene index
+    }
+
+    public void loadGame()
+    {
+        SceneManager.LoadScene("LoadScreen");
+    }
+
+    public void settings()
+    {
+        SceneManager.LoadScene("SetScreen");
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
