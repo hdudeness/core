fff6eeb4e0a13b610db8d0709e238fa9dc2b295e
Christian <ctbenven@mtu.edu>
2019-10-17 21:32:05 -0400

    sceen/button fix up

---
+++ Assets/Scripts/InGameScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameScript : MonoBehaviour
+{
+    public void menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
