0fcaf87e3db2c642753f6af6974e98930980a4d6
Nam Nguyen <54714630+namnguyennhn@users.noreply.github.com>
2019-10-24 00:04:32 -0400

    movement of enemy

---
+++ Assets/Scripts/EnemyManagement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyManagement : MonoBehaviour
+{
+    public EnemyTriangle enemyTriangle;
+    public EnergyCore energyCore;
+    public Transform enemyPosition;
+
+    private System.Random rand = new System.Random();
+
+    IEnumerator EnemyTriangleProducer()
+    {
+        while (true)
+        {
+            int direction = rand.Next(1, 3);
+            yield return new WaitForSeconds(1);
+
+            if(direction == 1)
+            {
+                EnemyTriangle newEnemy = Instantiate(enemyTriangle, new Vector3(5, rand.Next(0, 7), 0), Quaternion.identity);
+                newEnemy.endingPosition = energyCore.transform.position;
+            }
+            else
+            {
+                EnemyTriangle newEnemy = Instantiate(enemyTriangle, new Vector3(-5, rand.Next(0, 7), 0), Quaternion.identity);
+                newEnemy.endingPosition = energyCore.transform.position;
+            }
+
+            
+            
+        }
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyTriangleProducer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/EnemyTriangle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTriangle : MonoBehaviour
+{
+    public Vector3 startingPosition;
+    public Vector3 endingPosition;
+    public float travelTime;
+    public int health;
+    public int damgePerBullet;
+    public SpriteRenderer healthBar;
+    private float maxHealth;
+    float timeSinceSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.size = new Vector2(26 * (health / maxHealth), healthBar.size.y);
+        timeSinceSpawn += Time.fixedDeltaTime;
+        transform.position = Vector3.Lerp(startingPosition, endingPosition, timeSinceSpawn / travelTime);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    public void BulletHit()
+    {
+        health -= damgePerBullet;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
