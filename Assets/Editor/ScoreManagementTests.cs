cf4cf28cab61173945832b2f986434b04aaaab1b
Harry Taylor <35974116+hdudeness@users.noreply.github.com>
2019-10-31 18:20:45 -0400

    added testing suite
    To run testing suite in Unity:
    - Window -> General -> Test Runner
    - Switch to the EditMode tab
    - Hit "Run All"

---
+++ Assets/Editor/ScoreManagementTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class ScoreManagementTests
+{
+    [Test]
+    public void AddPointsOnEnemyKill_Test()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Test]
+    public void EnemyHealthLoweredWhenBulletHitsEnemy_Test()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Test]
+    public void ShieldCollisionKillsEnemy_Test()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
