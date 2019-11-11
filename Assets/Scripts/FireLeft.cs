016a94d37c1697fdb39b9749aff3ec59379afcf5
Nam Nguyen <54714630+namnguyennhn@users.noreply.github.com>
2019-11-11 01:19:15 -0500

    FireLeft control update

--- Assets/Scripts/FireLeft.cs
+++ Assets/Scripts/FireLeft.cs
@@ -7,6 +7,8 @@ public class FireLeft : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     private bool isPressed = false;
     public Shield shieldControl;
+    private float timeSinceLastShot = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,17 @@ public class FireLeft : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         if (isPressed)
         {
-            shieldControl.FireBullet();
+            Debug.Log(timeSinceLastShot);
+            if(timeSinceLastShot > 0 && timeSinceLastShot < 0.2)
+            {
+                timeSinceLastShot += Time.deltaTime;
+            }
+            else
+            {
+                shieldControl.FireBullet();
+                timeSinceLastShot = 0.1f;
+            }
+            
         }
     }
 
@@ -29,5 +41,6 @@ public class FireLeft : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public void OnPointerUp(PointerEventData eventData)
     {
         isPressed = false;
+        timeSinceLastShot = 0;
     }
 }
