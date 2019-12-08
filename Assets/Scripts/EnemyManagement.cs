32354c36a4e4e86242ee1c999d6360c6e182fbb7
kajackso <kajackso@mtu.edu>
2019-12-08 14:45:44 -0500

    Balance & Spawn positions updated
    Updates:
      - Enemy hearts and triangles now spawn from a height of -7 to 7 instead of the previous 0 to 7.
      - Enemy spawn counts have been changed to be more fair and progressive. Some levels have specialized unit mixes. Currently covers 15 levels, with the 15th level setting the enemy limits for the remainder of the run.
      - Adjusted heart healing range and rate
      - Hard capped heart enemy numbers at 3
      - Increased turret fire rate
      - New Game button changed text to Play
      - Load game button text changed to shop
    
    Removals:
      - Removed loadscreen scene
      - Removed levelselect scene

--- Assets/Scripts/EnemyManagement.cs
+++ Assets/Scripts/EnemyManagement.cs
@@ -17,9 +17,9 @@ public class EnemyManagement : MonoBehaviour
     private bool isEnemyHeartPinkSpawned = false;
     private bool isEnemyCircleMaroonSpawned = false;
     public int enemyTriangleCount = 0; 
-    public int enemyTriangleLimit = 20;
+    public int enemyTriangleLimit = 8;
     public int enemyOctagonPurpleCount = 0;
-    public int enemyOctagonPurpleLimit = 5;
+    public int enemyOctagonPurpleLimit = 3;
     public int enemyHeartPinkCount = 0;
     public int enemyHeartPinkLimit = 5;
     public int enemyCircleMaroonCount = 0;
@@ -35,13 +35,13 @@ public class EnemyManagement : MonoBehaviour
             yield return new WaitForSeconds(spawnTime);
             if (direction == 1 && enemyTriangleCount < enemyTriangleLimit)
             {
-                EnemyTriangle newEnemy = Instantiate(enemyTriangle, new Vector3(10, rand.Next(0, 7), 0), Quaternion.identity);
+                EnemyTriangle newEnemy = Instantiate(enemyTriangle, new Vector3(10, 7 - rand.Next(0, 14), 0), Quaternion.identity);
                 newEnemy.endingPosition = energyCore.transform.position;
                 enemyTriangleCount++;
             }
             else if (enemyTriangleCount < enemyTriangleLimit)
             {
-                EnemyTriangle newEnemy = Instantiate(enemyTriangle, new Vector3(-10, rand.Next(0, 7), 0), Quaternion.identity);
+                EnemyTriangle newEnemy = Instantiate(enemyTriangle, new Vector3(-10, 7 - rand.Next(0, 14), 0), Quaternion.identity);
                 newEnemy.endingPosition = energyCore.transform.position;
                 enemyTriangleCount++;
             }
@@ -59,13 +59,13 @@ public class EnemyManagement : MonoBehaviour
                 int direction = rand.Next(1, 3);    //Enemy will be spawned in the left or the right of the screen
                 if (direction == 1)
                 {
-                    EnemyOctagonPurple newEnemy = Instantiate(enemyOctagonPurple, new Vector3(10, rand.Next(0, 7), 0), Quaternion.identity);
+                    EnemyOctagonPurple newEnemy = Instantiate(enemyOctagonPurple, new Vector3(10, 7 - rand.Next(0, 14), 0), Quaternion.identity);
                     newEnemy.endingPosition = energyCore.transform.position;
                     enemyOctagonPurpleCount++;
                 }
                 else
                 {
-                    EnemyOctagonPurple newEnemy = Instantiate(enemyOctagonPurple, new Vector3(-10, rand.Next(0, 7), 0), Quaternion.identity);
+                    EnemyOctagonPurple newEnemy = Instantiate(enemyOctagonPurple, new Vector3(-10, 7 - rand.Next(0, 14), 0), Quaternion.identity);
                     newEnemy.endingPosition = energyCore.transform.position;
                     enemyOctagonPurpleCount++;
                 }
@@ -86,13 +86,13 @@ public class EnemyManagement : MonoBehaviour
 
                 if (direction == 1)
                 {
-                    EnemyHeartPink newEnemy = Instantiate(enemyHeartPink, new Vector3(10, rand.Next(0, 7), 0), Quaternion.identity);
+                    EnemyHeartPink newEnemy = Instantiate(enemyHeartPink, new Vector3(10, 7 - rand.Next(0, 14), 0), Quaternion.identity);
                     newEnemy.endingPosition = energyCore.transform.position;
                     enemyHeartPinkCount++;
                 }
                 else
                 {
-                    EnemyHeartPink newEnemy = Instantiate(enemyHeartPink, new Vector3(-10, rand.Next(0, 7), 0), Quaternion.identity);
+                    EnemyHeartPink newEnemy = Instantiate(enemyHeartPink, new Vector3(-10, 7 - rand.Next(0, 14), 0), Quaternion.identity);
                     newEnemy.endingPosition = energyCore.transform.position;
                     enemyHeartPinkCount++;
                 }
@@ -112,13 +112,13 @@ public class EnemyManagement : MonoBehaviour
 
                 if (direction == 1)
                 {
-                    EnemyCircleMaroon newEnemy = Instantiate(enemyCircleMaroon, new Vector3(10, rand.Next(0, 7), 0), Quaternion.identity);
+                    EnemyCircleMaroon newEnemy = Instantiate(enemyCircleMaroon, new Vector3(10, 7 - rand.Next(0, 14), 0), Quaternion.identity);
                     newEnemy.endingPosition = energyCore.transform.position;
                     enemyCircleMaroonCount++;
                 }
                 else
                 {
-                    EnemyCircleMaroon newEnemy = Instantiate(enemyCircleMaroon, new Vector3(-10, rand.Next(0, 7), 0), Quaternion.identity);
+                    EnemyCircleMaroon newEnemy = Instantiate(enemyCircleMaroon, new Vector3(-10, 7 - rand.Next(0, 14), 0), Quaternion.identity);
                     newEnemy.endingPosition = energyCore.transform.position;
                     enemyCircleMaroonCount++;
                 }
@@ -138,26 +138,104 @@ public class EnemyManagement : MonoBehaviour
     {
         if (gameData.level >= 2 && !isEnemyOctagonPurpleSpawned)
         {
-            enemyTriangleLimit = 25;
+            enemyTriangleLimit = 12;
             StartCoroutine(EnemyOctagonPurpleProducer());
             isEnemyOctagonPurpleSpawned = true;
         }
 
         if (gameData.level >= 3 && !isEnemyHeartPinkSpawned)
         {
-            enemyTriangleLimit = 20;
-            enemyOctagonPurpleLimit = 5;
+            enemyTriangleLimit = 15;
+            enemyOctagonPurpleLimit = 4;
             StartCoroutine(EnemyHeartPinkProducer());
             isEnemyHeartPinkSpawned = true;
         }
 
         if (gameData.level >= 4 && !isEnemyCircleMaroonSpawned)
         {
-            enemyTriangleLimit = 15;
+            enemyTriangleLimit = 10;
             enemyOctagonPurpleLimit = 5;
-            enemyHeartPinkLimit = 5;
+            enemyHeartPinkLimit = 2;
             StartCoroutine(EnemyCircleMaroonProducer());
             isEnemyCircleMaroonSpawned = true;
+        }
+
+        if (gameData.level >= 5)
+        {
+            enemyTriangleLimit      = 8;
+            enemyOctagonPurpleLimit = 6;
+            enemyHeartPinkLimit     = 3;
+            enemyCircleMaroonLimit  = 5;
+        }
+
+        if (gameData.level >= 6)
+        {
+            enemyTriangleLimit      = 8;
+            enemyOctagonPurpleLimit = 6;
+            enemyCircleMaroonLimit  = 8;
+        }
+
+        if (gameData.level >= 7)
+        {
+            enemyTriangleLimit      = 8;
+            enemyOctagonPurpleLimit = 7;
+            enemyCircleMaroonLimit  = 8;
+        }
+
+        if (gameData.level >= 8)
+        {
+            enemyTriangleLimit      = 30;
+            enemyOctagonPurpleLimit = 2;
+            enemyCircleMaroonLimit  = 0;
+        }
+
+        if (gameData.level >= 9)
+        {
+            enemyTriangleLimit      = 8;
+            enemyOctagonPurpleLimit = 6;
+            enemyCircleMaroonLimit  = 12;
+        }
+
+        if (gameData.level >= 10)
+        {
+            enemyTriangleLimit      = 3;
+            enemyOctagonPurpleLimit = 10;
+            enemyCircleMaroonLimit  = 0;
+        }
+
+        if (gameData.level >= 11)
+        {
+            enemyTriangleLimit      = 3;
+            enemyOctagonPurpleLimit = 0;
+            enemyCircleMaroonLimit  = 20;
+        }
+
+        if (gameData.level >= 12)
+        {
+            enemyTriangleLimit      = 10;
+            enemyOctagonPurpleLimit = 6;
+            enemyCircleMaroonLimit  = 3;
+        }
+
+        if (gameData.level >= 13)
+        {
+            enemyTriangleLimit      = 10;
+            enemyOctagonPurpleLimit = 6;
+            enemyCircleMaroonLimit  = 10;
+        }
+
+        if (gameData.level >= 14)
+        {
+            enemyTriangleLimit      = 14;
+            enemyOctagonPurpleLimit = 8;
+            enemyCircleMaroonLimit  = 8;
+        }
+
+        if (gameData.level >= 15)
+        {
+            enemyTriangleLimit      = 20;
+            enemyOctagonPurpleLimit = 8;
+            enemyCircleMaroonLimit  = 12;
         }
     }
 }
